d7e070ff67fa95e94c88c928bedecd71c2b4f675
I P <saserty@ukr.net>
2019-03-20 12:18:26 +0200

    reporting to HTML with no 3rd party tools

--- testsonjava/src/test/java/org/imaginarium/selenium/my/first/tests/on/java/BaseTest.java
+++ testsonjava/src/test/java/org/imaginarium/selenium/my/first/tests/on/java/BaseTest.java
@@ -1,22 +1,44 @@
 package org.imaginarium.selenium.my.first.tests.on.java;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.*;
+import org.junit.rules.TestRule;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
+import java.awt.*;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
     protected static WebDriver driver;
 
+    static File juReport;
+    static BufferedWriter juWriter;
+
     @BeforeClass
-    public static void setUp() {
+    public static void setUp() throws IOException {
+
+        //region HTML Report
+        String projectDir = System.getProperty("user.dir");
+        String juReportFile = projectDir + "\\ReportFile.html";
+        DateFormat dateFormat = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+        Date date = new Date();
+        juReport = new File(juReportFile);
+        juWriter = new BufferedWriter(new FileWriter(juReport, true));
+        juWriter.write("<html><body>");
+        juWriter.write("<h1>Test Execution Report - " + dateFormat.format(date)
+                + "</h1>");
+        //endregion
+
         WebDriverManager.chromedriver().setup();
 //        WebDriverManager.iedriver().arch32().setup();
 //        System.setProperty("webdriver.chrome.logfile", "chromedriver.log");
@@ -38,9 +60,54 @@ public class BaseTest {
     }
 
     @AfterClass
-    public static void tearDown() {
-        driver.close();
-        driver.quit();
+    public static void tearDown() throws IOException {
+
+        //region HTML Report
+        juWriter.write("</body></html>");
+        juWriter.close();
+        Desktop.getDesktop().browse(juReport.toURI());
+        //endregion
+
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
     }
 
+    //region HTML Report Watcher
+    @Rule
+    public TestRule testWatcher = new TestWatcher() {
+
+        @Override
+        public Statement apply(Statement base, Description description) {
+            return super.apply(base, description);
+        }
+
+        @Override
+        protected void succeeded(Description description) {
+            try {
+                juWriter.write(description.getDisplayName().split("\\(", 2)[0] + " "
+                        + "Passed"); // html
+                System.out.println(description.getDisplayName().split("\\(", 2)[0] + " "
+                        + "Passed"); // console
+                juWriter.write("<br/>");
+            } catch (Exception e1) {
+                System.out.println(e1.getMessage());
+            }
+        }
+
+        @Override
+        protected void failed(Throwable e, Description description) {
+            try {
+                juWriter.write(description.getDisplayName().split("\\(", 2)[0] + " "
+                        + e.getClass().getSimpleName() + " " + e.getMessage()); // html
+                System.out.println(description.getDisplayName().split("\\(", 2)[0] + " "
+                        + e.getClass().getSimpleName() + " " + e.getMessage()); // console
+                juWriter.write("<br/>");
+            } catch (Exception e2) {
+                System.out.println(e2.getMessage());
+            }
+        }
+    };
+    //endregion
 }
